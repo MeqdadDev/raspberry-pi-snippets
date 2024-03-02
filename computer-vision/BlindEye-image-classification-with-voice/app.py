368770ee77e47e9aea8b8b5637f99cada105f7bf
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-02 22:50:17 +0200

    computer-vision: update blind eye app file

--- computer-vision/BlindEye-image-classification-with-voice/app.py
+++ computer-vision/BlindEye-image-classification-with-voice/app.py
@@ -1,13 +1,13 @@
 '''
 By: MeqdadDev
 '''
+
 from teachable_machine_lite import TeachableMachineLite
 import cv2 as cv
 import pytesseract as ocr
 import pyttsx3   #  For Text to Speech
 
 cap = cv.VideoCapture(0)
-
 tts = pyttsx3.init()
 tts.setProperty('volume',1.0)
 
@@ -18,47 +18,68 @@ labels_path = "modelFiles/labels.txt"
 tm_model = TeachableMachineLite(model_path=model_path, labels_file_path=labels_path)
 ctr_frm = 0
 ctr_tts = 0
-
-label = None
+label = ""
 confidence = 0
-
-def extract_ocr(img):
-    _, binary_image = cv.threshold(img, 5, 250, cv.THRESH_BINARY)
-    # cv.imshow("Binary", binary_image)
-    return ocr.image_to_string(binary_image)
+defined_words = ["exit", "left", "stop", "right"]
 
 def say_label(label):
     tts.say(label)
     tts.runAndWait()
 
+def selectOCR(img):
+    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, binary_image = cv.threshold(img, 0, 255, cv.THRESH_BINARY)
+    recognized_text = ocr.image_to_string(binary_image).lower()
+    if "ex" in recognized_text:
+        say_label("exit")
+        return True, recognized_text
+    elif "st" in recognized_text:
+        say_label("stop")
+        return True, recognized_text
+    elif "le" in recognized_text:
+        say_label("left")
+        return True, recognized_text
+    elif "ri" in recognized_text:
+        say_label("right")
+        return True, recognized_text
+    else:
+        return False, recognized_text
+
+
 while True:
     ret, frame = cap.read()
-    if ctr_frm > 20:
-        if "exit" in str(extract_ocr(frame)).lower():
-            say_label("Go to Exit")
+    if ctr_frm > 23:
+        ctr_frm=0
+        isText, text = selectOCR(frame)
+        print("isText, text", isText, text)
+        if isText:
+            print(">> OCR:", text, end="\n\n")
+            confidence = 0.0
+            continue
         cv.imwrite(image_file_name, frame)
         results = tm_model.classify_frame(image_file_name)
         print("Label:", results["label"])
-        print("Confidence:", results["confidence"])
+        print("Confidence:", results["confidence"], end="\n\n")
         label = results["label"]
         confidence = results["confidence"]
-        id = results["id"]
-        ctr_frm=0
+        if label == "Gray" or label == "Black":
+            label += " T shirt"
     else:
         ctr_frm+=1
 
-    if confidence > 90.00:
-        cv.putText(frame, f"You see:{label.capitalize()}", (15,40), cv.FONT_ITALIC, 1, (0, 255, 0), 2)
+    if confidence > 97.00:
+        cv.putText(frame, f"You see: {label.capitalize()}", (15,40), cv.FONT_ITALIC, 1, (0, 255, 0), 2)
 
-        if (ctr_tts > 140):
+        if (ctr_tts > 30) and "Background" not in label:
             say_label(label)
             ctr_tts=0
         else:
             ctr_tts+=1
     cv.imshow('Blind Eye', frame)
-    print(ctr_frm)
+    # cv.putText(frame, f"You {cvText}", (15,40), cv.FONT_ITALIC, 1, (0, 255, 0), 2)
 
     k = cv.waitKey(1)
-
     if k%255 == 27:
         break
+
+cap.release()
